ab22e3ecb5ee5975ff020331c3aa31524e74cef0
Jonathan Washington <Jon.Washington@outlook.com>
2019-12-17 16:00:42 -0600

    light/dark logo

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,31 @@
-function submitForm() {
+submitForm = () => {
     $('form[name="mc-embedded-subscribe-form"]').submit();
     $('input[type="email"], textarea').val('');
 }
 
 const homeNavBarAtTop = 'navbar navbar-expand-lg navbar-dark bg-transparent fixed-top pb-5';
-const homeNavBarBelow = 'navbar navbar-expand-lg navbar-dark fixed-top about-nav';
+const homeNavBarBelow = 'navbar navbar-expand-lg navbar-light fixed-top about-nav bg-white';
+const logoPath = './src/imgs/logo';
+
+getLogoLight = () => {
+    return logoPath + 'light.png';
+}
+
+getLogoDark = () => {
+    return logoPath + 'dark.png';
+}
 window.addEventListener('scroll', function(e) {
     const navBar = $('#hnavBar');
     const navHeight = navBar.height()
     const navBottom = $(window).scrollTop() + navHeight;
-
     navBar.removeClass();
     if (navBottom > 800) {
         navBar.addClass(homeNavBarBelow);
+        $('#navlogo').attr('src', getLogoDark());
+
     } else {
         navBar.addClass(homeNavBarAtTop);
+        $('#navlogo').attr('src', getLogoLight());
+
     }
 })
