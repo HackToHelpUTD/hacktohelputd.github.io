785520c77d5ae7a20988253e409649adf0d9f6a1
Jonathan Washington <Jon.Washington@outlook.com>
2019-12-17 15:47:02 -0600

    fixed navbar top and color change on home page

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,19 @@
 function submitForm() {
     $('form[name="mc-embedded-subscribe-form"]').submit();
     $('input[type="email"], textarea').val('');
-  }
+}
+
+const homeNavBarAtTop = 'navbar navbar-expand-lg navbar-dark bg-transparent fixed-top pb-5';
+const homeNavBarBelow = 'navbar navbar-expand-lg navbar-dark fixed-top about-nav';
+window.addEventListener('scroll', function(e) {
+    const navBar = $('#hnavBar');
+    const navHeight = navBar.height()
+    const navBottom = $(window).scrollTop() + navHeight;
+
+    navBar.removeClass();
+    if (navBottom > 800) {
+        navBar.addClass(homeNavBarBelow);
+    } else {
+        navBar.addClass(homeNavBarAtTop);
+    }
+})
