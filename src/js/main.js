6473a836974902e41fc466bc1ab2bd96f12c89f5
Jonathan Washington <Jon.Washington@outlook.com>
2019-12-19 11:29:22 -0600

    navbar change color improved.

--- src/js/main.js
+++ src/js/main.js
@@ -19,15 +19,21 @@ getLogoDark = () => {
 
 window.onload = () => {
     window.navBar = $("#hnavBar");
+    window.navState = null;
     window.addEventListener("scroll", function(e) {
         const navHeight = window.navBar.height();
         const navBottom = $(window).scrollTop() + navHeight;
         const backGroundHeight = $(".officer-backdrop").height();
-        navBar.removeClass();
         if (navBottom > backGroundHeight) {
-            navBar.addClass(homeNavBarBelow);
-            $("#navlogo").attr("src", getLogoDark());
-        } else {
+            if (window.navState !== 0) {
+                navBar.removeClass();
+                navBar.addClass(homeNavBarBelow);
+                $("#navlogo").attr("src", getLogoDark());
+                window.navState = 0;
+            }
+        } else if (window.navState !== 1) {
+            navBar.removeClass();
+            window.navState = 1;
             navBar.addClass(homeNavBarAtTop);
             $("#navlogo").attr("src", getLogoLight());
         }
