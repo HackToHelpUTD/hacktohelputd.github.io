8ce3d3885108dac466e86f582140563889f7efb6
Jonathan Washington <Jon.Washington@outlook.com>
2019-12-18 19:13:36 -0600

    navbar and opt code changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,35 @@
 submitForm = () => {
     $('form[name="mc-embedded-subscribe-form"]').submit();
-    $('input[type="email"], textarea').val('');
-}
+    $('input[type="email"], textarea').val("");
+};
 
-const homeNavBarAtTop = 'navbar navbar-expand-lg navbar-dark bg-transparent fixed-top pb-5';
-const homeNavBarBelow = 'navbar navbar-expand-lg navbar-light fixed-top bg-white shadow-sm';
-const logoPath = './src/imgs/logo';
+const homeNavBarAtTop =
+    "navbar navbar-expand-lg navbar-dark bg-transparent fixed-top pb-5";
+const homeNavBarBelow =
+    "navbar navbar-expand-lg navbar-light fixed-top bg-white shadow-sm";
+const logoPath = "./src/imgs/logo";
 
 getLogoLight = () => {
-    return logoPath + 'light.png';
-}
+    return logoPath + "light.png";
+};
 
 getLogoDark = () => {
-    return logoPath + 'dark.png';
-}
-window.addEventListener('scroll', function(e) {
-    const navBar = $('#hnavBar');
-    const navHeight = navBar.height()
-    const navBottom = $(window).scrollTop() + navHeight;
-    navBar.removeClass();
-    if (navBottom > 800) {
-        navBar.addClass(homeNavBarBelow);
-        $('#navlogo').attr('src', getLogoDark());
+    return logoPath + "dark.png";
+};
 
-    } else {
-        navBar.addClass(homeNavBarAtTop);
-        $('#navlogo').attr('src', getLogoLight());
-
-    }
-})
+window.onload = () => {
+    window.navBar = $("#hnavBar");
+    window.addEventListener("scroll", function(e) {
+        const navHeight = window.navBar.height();
+        const navBottom = $(window).scrollTop() + navHeight;
+        const backGroundHeight = $(".officer-backdrop").height();
+        navBar.removeClass();
+        if (navBottom > backGroundHeight) {
+            navBar.addClass(homeNavBarBelow);
+            $("#navlogo").attr("src", getLogoDark());
+        } else {
+            navBar.addClass(homeNavBarAtTop);
+            $("#navlogo").attr("src", getLogoLight());
+        }
+    });
+};
