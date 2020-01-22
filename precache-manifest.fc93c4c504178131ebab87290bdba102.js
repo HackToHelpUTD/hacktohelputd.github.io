c2fbb38c18213b5dfecc0885049174c223c55560
Jon3123 <39013026+Jon3123@users.noreply.github.com>
2020-01-22 03:11:07 +0000

    Deploying to react-build from  - 9a7f7e637520f5329f920cecf4f475c676256fdc 🚀

---
+++ precache-manifest.fc93c4c504178131ebab87290bdba102.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79e693581fbb9d1ca2dd2075d21e7130",
+    "url": "/index.html"
+  },
+  {
+    "revision": "868ee916c5eb4218a3dd",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "4d44bfd9b28a518b6861",
+    "url": "/static/css/main.537d527b.chunk.css"
+  },
+  {
+    "revision": "868ee916c5eb4218a3dd",
+    "url": "/static/js/2.0c5a9e5e.chunk.js"
+  },
+  {
+    "revision": "f2fdf08f80a213cb1d4238bd28a4c710",
+    "url": "/static/js/2.0c5a9e5e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4d44bfd9b28a518b6861",
+    "url": "/static/js/main.7a29c093.chunk.js"
+  },
+  {
+    "revision": "480a794663bece4903ca",
+    "url": "/static/js/runtime-main.78c77dcd.js"
+  },
+  {
+    "revision": "b11db321b385e044d2a53dde245c0ac6",
+    "url": "/static/media/Officers.b11db321.jpeg"
+  },
+  {
+    "revision": "c1722f736a0454c66c8e26487306912b",
+    "url": "/static/media/historybackground.c1722f73.jpeg"
+  },
+  {
+    "revision": "b16f48a8c6f17afdc9660b497f932e76",
+    "url": "/static/media/logodark.b16f48a8.png"
+  },
+  {
+    "revision": "57ad1ebf41dadd1f93c8cc8f0e4e1363",
+    "url": "/static/media/logolight.57ad1ebf.png"
+  },
+  {
+    "revision": "3aa662f3d7feb252ebad2ad02ab35971",
+    "url": "/static/media/oureventsbackground.3aa662f3.jpeg"
+  },
+  {
+    "revision": "00b6fa78c35acfd4b336881d46df4bfb",
+    "url": "/static/media/pivotal_logo.00b6fa78.png"
+  },
+  {
+    "revision": "7fbcf68eb5bb248f1d3d2d5caee4612f",
+    "url": "/static/media/ux_logo.7fbcf68e.png"
+  },
+  {
+    "revision": "72891186bf4f8140b4e32874c6c2a40c",
+    "url": "/static/media/witb_logo.72891186.png"
+  }
+]);
